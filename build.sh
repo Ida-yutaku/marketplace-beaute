d58620c16d2ac9ee0206c50ed9994e9ba8a79941
Yutaku <idayath.kalam@epitech.eu>
2026-08-12 23:34:40 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Se déplacer dans le dossier du backend Django
+cd backend
+
+# Installer les dépendances
 pip install -r requirements.txt
 
+# Collecter les fichiers statiques et faire les migrations
 python manage.py collectstatic --no-input
 python manage.py migrate
